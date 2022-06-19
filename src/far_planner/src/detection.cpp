96e17249b520a816d7fed4b45b94513abec86d70
Lee qun zhao <65322941+Leeable@users.noreply.github.com>
2022-06-19 18:34:49 +0800

    Add files via upload

---
+++ src/far_planner/src/detection.cpp
@@ -0,0 +1,188 @@
+#include "far_planner/detection.h"
+
+using namespace std;
+using namespace cv;
+using namespace dnn;
+
+Detection::Detection()
+{
+    m_width = 0;
+    m_height = 0;
+    m_inpWidth = 320;
+    m_inpHeight = 320;
+    m_confThro = 0.25;
+    m_NMSThro = 0.4;
+}
+
+Detection::~Detection(){};
+
+void Detection::Init( const DetectionParams& params) {
+    yl_params_ = params;
+    ReadModel();
+}
+
+
+void Detection::Dump()
+{
+    m_outs.clear();
+    m_boxes.clear();
+    m_confs.clear();
+    m_classIds.clear();
+    m_perfIndx.clear();
+}
+
+void Detection::Initalize(int width, int height)
+{
+    m_width = width;
+    m_height = height;
+}
+
+void Detection::ReadModel()
+{
+    // string classesFile = "/home/lqz/ros_ws/src/detection_pkg/data/coco.names";
+    // string modelConfig = "/home/lqz/ros_ws/src/detection_pkg/data/yolov4-tiny.cfg";
+    // string modelWeights = "/home/lqz/ros_ws/src/detection_pkg/data/yolov4-tiny.weights";
+
+    std::string classesFile = yl_params_.coconame;
+    ROS_INFO("this is:%s",classesFile.c_str());
+    std::string modelConfig = yl_params_.cfg;
+    std::string modelWeights = yl_params_.weights;
+    ifstream ifs(classesFile.c_str());
+    string line;
+    while (getline(ifs, line)){
+        m_classes.push_back(line);
+    }
+
+    m_model = readNetFromDarknet(modelConfig, modelWeights);
+    m_model.setPreferableBackend(DNN_BACKEND_OPENCV);
+    m_model.setPreferableTarget(DNN_TARGET_OPENCL);
+}
+
+bool Detection::Detecting(Mat frame)
+{
+    Dump();
+    m_frame = frame.clone();
+    blobFromImage(m_frame, m_blob, 1 / 255.0, Size(m_inpWidth, m_inpHeight));
+    m_model.setInput(m_blob);
+    m_model.forward(m_outs, GetOutputNames());
+    PostProcess();
+    Drawer();
+    return true;
+}
+
+vector<string> Detection::GetOutputNames()
+{
+    static vector<string> names;
+    if (names.empty())
+    {
+        vector<int> outLayers = m_model.getUnconnectedOutLayers();
+        vector<string> layersNames = m_model.getLayerNames();
+        names.resize(outLayers.size());
+        for (int i = 0; i < outLayers.size(); ++i)
+        {
+            names[i] = layersNames[outLayers[i] - 1];
+        }
+    }
+    return names;
+}
+
+void Detection::PostProcess()
+{
+    for (int num = 0; num < m_outs.size(); num++)
+    {
+        Point Position;
+        double confidence;
+        float *data = (float *)m_outs[num].data;
+        for (int j = 0; j < m_outs[num].rows; j++, data += m_outs[num].cols)
+        {
+            Mat scores = m_outs[num].row(j).colRange(5, m_outs[num].cols);
+            minMaxLoc(scores, 0, &confidence, 0, &Position);
+            if (confidence > m_confThro)
+            {
+                int centerX = (int)(data[0] * m_width);
+                int centerY = (int)(data[1] * m_height);
+                int width = (int)(data[2] * m_width);
+                int height = (int)(data[3] * m_height);
+                int left = centerX - width / 2;
+                int top = centerY - height / 2;
+
+                m_classIds.push_back(Position.x);
+                m_confs.push_back((float)confidence);
+                m_boxes.push_back(Rect(left, top, width, height));
+            }
+        }
+    }
+    NMSBoxes(m_boxes, m_confs, m_confThro, m_NMSThro, m_perfIndx);
+}
+
+void Detection::Drawer()
+{
+    for (int i = 0; i < m_perfIndx.size(); i++)
+    {
+        int idx = m_perfIndx[i];
+        Rect box = m_boxes[idx];
+        if (m_classIds[idx] == 0)
+        {
+            DrawBoxes(m_classIds[idx], m_confs[idx], box.x, box.y, box.x + box.width, box.y + box.height);
+        }
+    }
+}
+
+void Detection::DrawBoxes(int classId, float conf, int left, int top, int right, int bottom)
+{
+    rectangle(m_frame, Point(left, top), Point(right, bottom), Scalar(255, 178, 50), 3);
+    string label = format("%.2f", conf);
+    if (!m_classes.empty())
+    {
+        CV_Assert(classId < (int)m_classes.size());
+        label = m_classes[classId] + ":" + label;
+    }
+
+    int baseLine;
+    Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+    top = max(top, labelSize.height);
+    rectangle(m_frame, Point(left, top - round(1.5 * labelSize.height)), Point(left + round(1.5 * labelSize.width), top + baseLine), Scalar(255, 255, 255), FILLED);
+    putText(m_frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 0), 1);
+}
+
+Mat Detection::GetFrame()
+{
+    return m_frame;
+}
+
+string Detection::GetResult()
+{
+    string result = "";
+
+    //获取所有最佳检测框信息
+    for (int i = 0; i < m_perfIndx.size(); i++)
+    {
+        int idx = m_perfIndx[i];
+        Rect box = m_boxes[idx];
+
+        // std::cout << "m_classIds[idx]"<< m_classIds[idx] << std::endl;
+        // std::cout << "m_confs[idx]"<< m_confs[idx] << std::endl;
+        // std::cout << "box.x"<< box.x << std::endl;
+        // std::cout << "box.y"<< box.y << std::endl;
+        // std::cout << "box.width"<< box.width << std::endl;
+        // std::cout << "box.height"<< box.height << std::endl;
+
+        if (m_classIds[idx] == 0) //仅person类
+        {
+            stringstream strStream;
+            // int、float类型都可以塞到stringstream中
+            strStream << m_classIds[idx] << "," << m_confs[idx] << "," << box.x << "," << box.y << "," << box.width << "," << box.height;
+            result = strStream.str();
+        }
+    }
+
+    return result;
+}
+
+int Detection::GetResWidth(){
+    return m_width;
+}
+
+int Detection::GetResHeight(){
+    return m_height;
+}
